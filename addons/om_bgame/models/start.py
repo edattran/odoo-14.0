7c44374b3947c72b392339e4b7332fc20e4b1d53
Rangith Edattale <edattran@students.zhaw.ch>
2021-04-11 22:29:08 +0200

    - start implementation of start game feature - post request with user credentials created

--- addons/om_bgame/models/start.py
+++ addons/om_bgame/models/start.py
@@ -1,13 +1,24 @@
 from odoo import api, fields, models
 import requests
+import json
 
 
 class BgameStart(models.Model):
     _name = 'bgame.start'
     _description = 'Business Game Start Record'
 
-    player_name = fields.Char(string='Name', required=True)
+    player_name = fields.Char(string='Nick Name', required=True)
+    database_name = fields.Char(string='DB Name', required=True)
+    player_mail = fields.Char(string='Mail Address', required=True)
+    player_password = fields.Char(string='Password', required=True)
+    player_status = fields.Char(string="Game Status")
 
-    def get_rest(self):
-        response = requests.get("http://localhost:8070/api/users")
-        print(response.text)
+    def start_game(self):
+        url = 'http://localhost:8070/customapi/startGame'
+        myobj = {'playerName': self.player_name,
+                 'databaseName': self.database_name,
+                 'playerMail': self.player_mail,
+                 'playerPassword': self.player_password,
+                 }
+        reply = requests.post(url, json=myobj)
+        print(reply)
