732a1aaf55a93b0cd443249f3980c31d1873bb84
Rangith Edattale <edattran@students.zhaw.ch>
2021-04-26 00:17:23 +0200

    purchse great protein

--- addons/om_bgame/models/purchase.py
+++ addons/om_bgame/models/purchase.py
@@ -1,27 +1,38 @@
 from odoo import api, fields, models
 import requests
+import time
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     def send_quotation(self):
-        url = '/customapi/venGreatProtein'
+        url = '/customapi/newOrder'
         line = self.env['purchase.order.line'].browse(self.id)
         start_game = self.env['bgame.start'].search([('player_status', '=', 'active')])
-        myobj = {'productName': line.name,
-                 'quantity': line.product_qty,
-                 'start': {
-                     'id': start_game.player_extern_id
+        myobj = {'id': self.name,
+                 'reqQty': line.product_qty,
+                 'greatProtein': {
+                     'productName': line.name,
+                     'start': {
+                         'id': start_game.player_extern_id
+                     }
                  }}
         reply = requests.post(start_game.spring_url + url, json=myobj)
-        print(reply)
         for order in self:
             order.write({'state': 'sent'})
+        print(reply)
+
+
+    def receive_order(self):
+        self.button_confirm()
+        print('fuck')
+        return True
+
 
     def button_confirm(self):
         for order in self:
-            if order.state not in ['sent']:
+            if order.state not in ['draft', 'sent']:
                 continue
             order._add_supplier_to_product()
             # Deal with double validation process
