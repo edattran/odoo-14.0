d40ac6151659c426a6f4435c78a319dd419678de
Rangith Edattale <edattran@students.zhaw.ch>
2021-05-13 13:08:32 +0200

    started with initital setup

--- addons/om_bgame/models/start.py
+++ addons/om_bgame/models/start.py
@@ -30,14 +30,107 @@ class BgameStart(models.Model):
         print(reply)
 
     def initialize_game(self):
-        # self.env['product.product'].create({'name': 'Test product2',
-        #                                     'type': 'product',
-        #                                     'list_price': '1',
-        #                                     'sale_ok': 'false',
-        #                                     'purchase_ok': 'true'
-        #                                     })
+        product1 = self.env['product.product'].create({'name': 'Aspartame',
+                                                       'type': 'product',
+                                                       'list_price': '1',
+                                                       'sale_ok': 'false',
+                                                       'purchase_ok': 'true'
+                                                       })
+        product1_tmpl_int = int(product1.product_tmpl_id)
+        product1_id_int = int(product1.id)
+        vendor1 = self.env['res.partner'].create({'name': 'Super Sweet',
+                                                  'display_name': 'Super Sweet'})
+        vendor1_int = int(vendor1.id)
+        self.env['product.supplierinfo'].create({'name': vendor1_int,
+                                                 'price': '10.00',
+                                                 'product_tmpl_id': product1_tmpl_int})
 
-        self.env['mrp.bom'].create({'product_qty': '42'})
+        product2 = self.env['product.product'].create({'name': 'Whey Isolate',
+                                                       'type': 'product',
+                                                       'list_price': '1',
+                                                       'sale_ok': 'false',
+                                                       'purchase_ok': 'true'
+                                                       })
+        product2_tmpl_int = int(product2.product_tmpl_id)
+        product2_id_int = int(product2.id)
+        vendor2 = self.env['res.partner'].create({'name': 'Great Protein',
+                                                  'display_name': 'Great Protein'})
+        vendor2_int = int(vendor2.id)
+        self.env['product.supplierinfo'].create({'name': vendor2_int,
+                                                 'price': '13.00',
+                                                 'product_tmpl_id': product2_tmpl_int})
 
+        product3 = self.env['product.product'].create({'name': 'Cacao Powder',
+                                                       'type': 'product',
+                                                       'list_price': '1',
+                                                       'sale_ok': 'false',
+                                                       'purchase_ok': 'true'
+                                                       })
+        product3_tmpl_int = int(product3.product_tmpl_id)
+        product3_id_int = int(product3.id)
+        vendor3 = self.env['res.partner'].create({'name': 'Choco Loco',
+                                                  'display_name': 'Choco Loco'})
+        vendor3_int = int(vendor3.id)
+        self.env['product.supplierinfo'].create({'name': vendor3_int,
+                                                 'price': '40.00',
+                                                 'product_tmpl_id': product3_tmpl_int})
 
+        product4 = self.env['product.product'].create({'name': 'Pea Protein Isolate',
+                                                       'type': 'product',
+                                                       'list_price': '1',
+                                                       'sale_ok': 'false',
+                                                       'purchase_ok': 'true'
+                                                       })
+        product4_tmpl_int = int(product4.product_tmpl_id)
+        product4_id_int = int(product4.id)
+        vendor4 = self.env['res.partner'].create({'name': 'Healthy Nutrition',
+                                                  'display_name': 'Healthy Nutrition'})
+        vendor4_int = int(vendor4.id)
+        self.env['product.supplierinfo'].create({'name': vendor4_int,
+                                                 'price': '50.00',
+                                                 'product_tmpl_id': product4_tmpl_int})
 
+        pbom1 = self.env['product.product'].create({'name': 'Whey Bars',
+                                                    'type': 'product',
+                                                    'list_price': '3',
+                                                    'sale_ok': 'true',
+                                                    'purchase_ok': 'false'})
+        pbom1_tmpl_int = int(pbom1.product_tmpl_id)
+        bom1 = self.env['mrp.bom'].create({'product_qty': '46',
+                                           'product_tmpl_id': pbom1_tmpl_int})
+        self.env['mrp.bom.line'].create({'product_id': product1_id_int,
+                                         'product_qty': '1',
+                                         'bom_id': bom1.id})
+        self.env['mrp.bom.line'].create({'product_id': product2_id_int,
+                                         'product_qty': '1',
+                                         'bom_id': bom1.id})
+        self.env['mrp.bom.line'].create({'product_id': product3_id_int,
+                                         'product_qty': '1',
+                                         'bom_id': bom1.id})
+
+        pbom2 = self.env['product.product'].create({'name': 'Vegan Bars',
+                                                    'type': 'product',
+                                                    'list_price': '5',
+                                                    'sale_ok': 'true',
+                                                    'purchase_ok': 'false'})
+        pbom2_tmpl_int = int(pbom2.product_tmpl_id)
+        bom2 = self.env['mrp.bom'].create({'product_qty': '46',
+                                           'product_tmpl_id': pbom2_tmpl_int})
+        self.env['mrp.bom.line'].create({'product_id': product1_id_int,
+                                         'product_qty': '1',
+                                         'bom_id': bom2.id})
+        self.env['mrp.bom.line'].create({'product_id': product4_id_int,
+                                         'product_qty': '1',
+                                         'bom_id': bom2.id})
+        self.env['mrp.bom.line'].create({'product_id': product3_id_int,
+                                         'product_qty': '1',
+                                         'bom_id': bom2.id})
+
+        customer1 = self.env['res.partner'].create({'name': 'Protein Store',
+                                                  'display_name': 'Protein Store'})
+        customer2 = self.env['res.partner'].create({'name': 'Power Store',
+                                                    'display_name': 'Power Store'})
+        customer3 = self.env['res.partner'].create({'name': 'Nature Store',
+                                                    'display_name': 'Nature Store'})
+        customer4 = self.env['res.partner'].create({'name': 'Healty Vegan Store',
+                                                    'display_name': 'Healty Vegan Store'})
